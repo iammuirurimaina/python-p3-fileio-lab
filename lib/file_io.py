b9272e06ef57edf944c7f9a0c7826457b793370a
maina <ian.maina@student.moringaschool.com>
2023-09-19 16:47:51 +0300

    Assignment completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
 def write_file(file_name, file_content):
-    pass
+    file = f'{file_name}.txt'
+    with open(file, mode='w', encoding='utf-8') as log_file:
+        log_file.write(file_content)
+
+
 
 def append_file(file_name, append_content):
+    file = f'{file_name}.txt'
+    with open(file, mode='a', encoding='utf-8') as log_file:
+        log_file.write(append_content)
+        
     pass
 
 def read_file(file_name):
+    with open(f'{file_name}.txt', encoding='utf-8') as log_file:
+        return log_file.read()
     pass
